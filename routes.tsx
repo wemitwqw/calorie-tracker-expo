6bd38eea45a640feceef77aee234af26ff1da087
Vladislav Priske <55079623+wemitwqw@users.noreply.github.com>
2025-04-09 23:06:17 +0300

    rm routes file, using constants instead;

--- routes.tsx
+++
@@ -1,8 +0,0 @@
-export const ROUTES = {
-    LOGIN: '/(auth)/login',
-    REGISTER: '/(auth)/register',
-    HOME: '/(app)',
-    ADD_MEAL: '/(app)/add-meal',
-    WHITELIST: '/(app)/admin/whitelist',
-    CALLBACK: '/callback',
-} as any;
